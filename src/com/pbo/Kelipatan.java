8bafaff67a95f29dd3e34e8a74852c767ad1dc06
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-10-06 22:11:46 +0700

    Initial commit

---
+++ src/com/pbo/Kelipatan.java
@@ -0,0 +1,19 @@
+package com.pbo;
+
+/*
+ * # Author
+ * Nama  : Agung Sepruloh
+ * Kelas : IF-11K
+ * NIM   : 10118902
+ * Deskripsi program: program ini akan menampilkan bilangan kelipatan dimulai dari 3,5 sampai dengan 35
+ *
+ */
+
+public class Kelipatan {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(3.5 * i);
+        }
+    }
+}
